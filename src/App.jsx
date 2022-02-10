1c253a00c149d586e0b373ed4ffe9335dfd09d1b
Swanand-Wagh <swanandwagh7@gmail.com>
2022-02-10 21:16:12 +0530

    basic empty file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <h1>hey</h1>
+      <div>
+        <Routes></Routes>
+      </div>
     </>
   );
 };

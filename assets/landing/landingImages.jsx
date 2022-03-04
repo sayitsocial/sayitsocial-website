6b80159a3eb132102e5b00b7d89c133d8ec5f9d6
Bobby Kumar <kashyapashish0002@gmail.com>
2022-03-05 00:00:58 +0530

    Added images exports with jsx file

---
+++ assets/landing/landingImages.jsx
@@ -0,0 +1,37 @@
+import three from "./03.png";
+import award from "./award.svg";
+import blue from "./blue.png";
+import company from "./company.png";
+import corner from "./corner.png";
+import event from "./event.svg";
+import grow from "./grow.svg";
+import hero from "./hero.png";
+import know from "./know.svg";
+import meet from "./meet.png";
+import meetupEvent from "./meetup-event.png";
+import ngo from "./ngo.png";
+import people from "./people.svg";
+import social from "./social.png";
+import spread from "./spread.svg";
+import tfooe from "./The-Future-of-Online-Events.png";
+import track from "./track.svg";
+
+export default {
+  three,
+  award,
+  blue,
+  company,
+  corner,
+  event,
+  grow,
+  hero,
+  know,
+  meet,
+  meetupEvent,
+  ngo,
+  people,
+  social,
+  spread,
+  tfooe,
+  track,
+};
